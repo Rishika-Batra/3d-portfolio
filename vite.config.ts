0394a5083d8a6fd3110bbcb6d70bdb4a2ae05345
Rishika-Batra <batrarishika06@gmail.com>
2026-06-29 20:03:22 +0530

    Fix base path for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/3d-portfolio/", // 👈 Add this line (matching your folder name '3d-portfolio')
+  base: "/", // 👈 Change this to "/" for Vercel
 });
